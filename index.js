c7bf4b115923a86ca69c073ca2a56ade39d6de5d
Leira C Sanchez Quinones <leira.sq@gmail.com>
2021-09-23 16:42:28 -0400

    all done except for 3

--- index.js
+++ index.js
@@ -23,8 +23,72 @@ button.addEventListener("click", () => {
 // #3 Follow me!
 // TODO
 
-// const followMe = document.getElementsByClassName("follow-me")[0];
-const followMe = document.getElementById("follow-me-parent");
-followMe.addEventListener("mouseover", () => {
+const followMeParent = document.getElementById("follow-me-parent");
+const followMe = document.getElementsByClassName("follow-me")[0];
+followMeParent.addEventListener("mouseover", () => {
   console.log("entered");
+});
+
+// #4 Children
+
+const hasChildrenUl = document.getElementsByClassName("has-children")[0];
+
+for (let i = 0; i < hasChildrenUl.children.length; i++) {
+  if (i % 2 === 0) {
+    hasChildrenUl.children.item(i).style.backgroundColor = "lime";
+  }
+}
+hasChildrenUl.firstElementChild.style.backgroundColor = "blue";
+hasChildrenUl.lastElementChild.style.backgroundColor = "palevioletred";
+
+// #5 Form Values
+
+const p = document.getElementsByClassName("wrote")[0];
+const input = document.querySelector("input");
+input.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  p.innerHTML = e.target.value;
+});
+
+// #6 Form Validation
+const value1 = document.querySelector("input[name=large-value]");
+value1.type = "number";
+const value2 = document.querySelector("input[name=smaller-value]");
+value2.type = "number";
+const label2 = document.querySelector("label[for=smaller-value]");
+
+const error = document.getElementsByClassName("error--hidden")[0];
+
+value1.addEventListener("input", (e) => {
+  value1.value = parseInt(e.target.value);
+  checkValues();
+});
+
+value2.addEventListener("input", (e) => {
+  value2.value = parseInt(e.target.value);
+  checkValues();
+});
+
+function checkValues() {
+  if (parseInt(value1.value) < parseInt(value2.value)) {
+    label2.style.color = "red";
+    error.classList.remove("error--hidden");
+  } else {
+    label2.style.color = "black";
+    error.classList.add("error--hidden");
+  }
+}
+
+// #7 Working with Objects
+const ulObject = document.getElementsByClassName("photos")[0];
+
+data.photos.forEach((row) => {
+  const rowList = document.createElement("li");
+  const rowImg = document.createElement("img");
+  const rowP = document.createElement("p");
+  rowP.innerText = row.author + " " + row.format;
+  rowImg.src = row.image_url;
+  rowList.appendChild(rowImg);
+  rowList.appendChild(rowP);
+  ulObject.appendChild(rowList);
 });
